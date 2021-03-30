d6cf2373953d855584d1761da88701d1c622e912
Alexander <a-l-e-k-s@yandex.ru>
2021-03-30 11:10:55 +0300

    Initial Commit

---
+++ ColorChangerHW3.2/ColorChangerHW3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangerHW3_2App.swift
+//  ColorChangerHW3.2
+//
+//  Created by Alexander Konovalov on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangerHW3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorChangerHW3.2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorChangerHW3.2
+//
+//  Created by Alexander Konovalov on 30.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
